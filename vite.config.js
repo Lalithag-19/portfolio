a5e32e47cba1bfabc83a02bf064bdbb3988a6968
Gowravarapu Lalitha <22bq1a4731@vvit.net>
2025-08-06 01:53:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This base property is the name of your GitHub repository.
+  // It's crucial for deploying to GitHub Pages.
+  base: '/portfolio/', 
   plugins: [react()],
 })
+
